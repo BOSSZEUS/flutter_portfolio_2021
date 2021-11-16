8744e3079059bfbf0f529a75465223a35975cf42
Jordan Scheftic <47509698+BOSSZEUS@users.noreply.github.com>
2021-11-15 17:55:41 -0800

    circleavatar done

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -6,34 +7,40 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color(0xff0F0F1E),
-        centerTitle: true,
-        title: const Text(
-          'Jordan Scheftic - FullStack Developer',
-          style: TextStyle(
-            fontFamily: 'Petemoss',
-            fontSize: 50,
-            fontWeight: FontWeight.bold,
-            color: Color(0xff4af1f2),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(45),
+        child: AppBar(
+          backgroundColor: const Color(0xff0F0F1E),
+          centerTitle: true,
+          title: AnimatedTextKit(
+            animatedTexts: [
+              TyperAnimatedText(
+                '< Jordan Scheftic - FullStack Developer >',
+                textStyle: const TextStyle(
+                  fontFamily: 'SpecialElite',
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+                speed: const Duration(milliseconds: 100),
+              ),
+            ],
+            isRepeatingAnimation: true,
           ),
         ),
       ),
       body: Row(
-        children: [
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                CircleAvatar(
-                  radius: 50,
-                  backgroundImage: AssetImage('images/profilepic.jpg'),
-                )
-              ],
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: const [
+          SafeArea(
+            child: CircleAvatar(
+              radius: 150,
+              backgroundColor: Colors.transparent,
+              child: CircleAvatar(
+                radius: 85,
+                backgroundImage: AssetImage('images/profilepic.jpg'),
+              ),
             ),
-          ),
-          Expanded(
-            child: Column(),
           ),
         ],
       ),
